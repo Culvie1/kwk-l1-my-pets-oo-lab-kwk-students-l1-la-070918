c0c83b1b7348eb7feac550bce98b6e6d0aa7645e
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-12 23:15:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-  # code goes here
+attr_accessor :purchase, :name, :mood 
+def initialize(purchase, name, mood)
+  @purchase = purchase
+  @name = name
+  @mood = mood
+end
+end
+  
+  dog_one = Dog.new("Shelter", "Lorken the Destroyer", "Happy")
+  
+puts dog_one.purchase
+puts dog_one.name
+puts dog_one.mood
 end
