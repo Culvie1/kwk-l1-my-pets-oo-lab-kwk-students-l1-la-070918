c70589c9ccb960f97bf4976a56ea76ad0055e496
Culvie1 <jordan.pomerantz@gmail.com>
2018-07-12 23:13:01 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,15 @@
 class Fish
-  # code goes here
+attr_accessor :purchase, :name, :mood 
+def initialize(purchase, name, mood)
+  @purchase = purchase
+  @name = name
+  @mood = mood
+end
+end
+  
+fish_one = Fish.new("Pet Store", "California Roll", "Content")
+  
+puts fish_one.purchase
+puts fish_one.name
+puts fish_one.mood
 end
